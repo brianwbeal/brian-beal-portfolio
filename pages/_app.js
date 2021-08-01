15e0a0d76995052dc2c9d86a3e4f67043fd59989
Brian Beal <brianwbeal@gmail.com>
2021-08-01 15:45:55 -0700

    Add necessary files to create-next-app boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+/*
+*
+*  NextJS Custom App
+*  Where ApolloProvider, ThemeProvider, GlobalStyle, and page views are all arranged
+* 
+*/
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import App from 'next/app';
+import React from 'react';
+// import 'cross-fetch/polyfill';
+// import { ApolloProvider } from '@apollo/react-hooks';
+// import ApolloClient from 'apollo-boost';
+// import theme from '../utils/theme';
+// import { ThemeProvider } from 'styled-components';
+// import GlobalStyle from '../components/layout/global-style';
+
+// import Header & Footer components
+
+// const client = new ApolloClient({
+//   uri: 'http://localhost:5000/graphql',
+// });
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <div>
+        <Component {...pageProps} />
+      </div>
+    )
+  }
 }
-
-export default MyApp
